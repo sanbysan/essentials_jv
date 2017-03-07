5eae0f66f3433914a7f844d1d341ffa7a787a42b
sanbysan <sandra.morawska@gmail.com>
2017-03-07 11:26:30 +0100

    wrpper

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-var arraySelect = document.getElementsByTagName('select');
 var element = document.getElementById('content');
 
-function dropdownStyles() {
-	var style = this.id;
-	var value = this.value;
-	
+function dropdownStyles(select) {
+	var style = select.id;
+	var value = select.value;
 	element.style[style] = value;
 }
-for (var i = 0; i < arraySelect.length; i++) {
-	arraySelect[i].addEventListener( 'change', dropdownStyles );
+
+function onWrapperClick(event) {
+	var target = event.target;
+	if(target.tagName == "SELECT") {
+		dropdownStyles(target);
+	}
 }
+
+var wrapper = document.getElementById("wrapper");
+wrapper.addEventListener('click', onWrapperClick);
+
